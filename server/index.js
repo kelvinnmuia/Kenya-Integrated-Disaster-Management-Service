111393aa52ccf90a289e64aa063a439553eb2323
kelvinnmuia <kelvinnmuia@gmail.com>
2024-07-23 14:43:20 +0300

    updated readme file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server')
-const PORT = process.env.PORT || 5000; // look for default port else set to 5000 for local
+const PORT = process.env.PORT || 5001; // look for default port else set to 5000 for local
 
 app.listen(PORT, ()=>{
     console.log(`Server started on PORT ${PORT}`);
